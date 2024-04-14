e8975b83a10cee42292d8ed7dcbc521ccfcb46a1
aakashthapa12 <aakashtm12@gmail.com>
2024-04-14 13:36:59 +0530

    Add update user API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -75,7 +75,7 @@ export const google = async(req, res, next) => {
             })
             await newUser.save();
             const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
-            const {password, ...rest} = user._doc;
+            const {password, ...rest} = newUser._doc;
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
             }).json(rest);
