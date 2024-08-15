6f4fa3689ea090b348d07752aac423236ff304c7
aakash.thapa <techaksh45@gmail.com>
2024-08-16 01:14:31 +0530

    show user posts inside dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -123,3 +123,4 @@ export const getposts = async (req, res, next) => {
     next(error);
   }
 };
+
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", 
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
 };
