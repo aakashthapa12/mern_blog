f523f8cee4254f690bf755f1be3f4c64d629d193
aakash.thapa <techaksh45@gmail.com>
2024-09-07 18:56:25 +0530

    complete about and projects pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,15 @@
-
+import CallToAction from "../components/CallToAction";
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning MERN!
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
