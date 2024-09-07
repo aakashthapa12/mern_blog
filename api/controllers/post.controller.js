ea4380d1eed5792845e15107cb538731003e080c
aakash.thapa <techaksh45@gmail.com>
2024-09-07 15:11:33 +0530

    complete home page

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,37 +1,3 @@
-// import { errorHandler } from "../utils/error.js";
-// import Post from "../models/post.model.js";
-
-// export const create = async (req, res, next) => {
-//   if (!req.user.isAdmin) {
-//     return next(errorHandler(403, "You are not allowed to create a post"));
-//   }
-//   if (!req.body.title || !req.body.content) {
-//     return next(errorHandler(400, "Please provide all required fields"));
-//   }
-//   const slug = req.body.title
-//     .split(" ")
-//     .join("-")
-//     .toLowerCase()
-//     .replace(/[^a-zA-Z0-9-]/g, "-");
-
-//   const newPost = new Post({
-//     ...req.body,
-//     slug,
-//     userId: req.user.id,
-//   });
-//   try {
-//     const savedPost = await newPost.save();
-//     res.save(201).json(savedPost);
-//   } catch (error) {
-//     // Handle the duplicate key error
-//     if (error.code === 11000) {
-//       // 11000 is the error code for duplicate key
-//       return next(errorHandler(400, "A post with this title already exists"));
-//     }
-//     next(error);
-//   }
-// };
-
 import { errorHandler } from "../utils/error.js";
 import Post from "../models/post.model.js";
 
@@ -79,50 +45,6 @@ export const create = async (req, res, next) => {
   }
 };
 
-// export const getposts = async (req, res, next) => {
-//   try {
-//     const startIndex = parseInt(req.query.startIndex) || 0;
-//     const limit = parseInt(req.query.limit) || 9;
-//     const sortDirection = req.query.order === "asc" ? 1 : -1;
-
-//     const posts = await Post.find({
-//       ...(req.query.userId && { userId: req.query.userId }),
-//       ...(req.query.category && { category: req.query.category }),
-//       ...(req.query.slug && { slug: req.query.slug }), // Fixed key to be 'slug' instead of 'category'
-//       ...(req.query.postId && { _id: req.query.postId }),
-//       ...(req.query.searchTerm && {
-//         $or: [
-//           { title: { $regex: req.query.searchTerm, $options: "i" } },
-//           { content: { $regex: req.query.searchTerm, $options: "i" } },
-//         ],
-//       }),
-//     })
-//       .sort({ updatedAt: sortDirection })
-//       .skip(startIndex)
-//       .limit(limit);
-
-//     const totalPosts = await Post.countDocuments();
-
-//     const now = new Date();
-//     const oneMonthAgo = new Date(
-//       now.getFullYear(),
-//       now.getMonth() - 1,
-//       now.getDate()
-//     );
-
-//     const lastMonthPosts = await Post.countDocuments({
-//       createdAt: { $gte: oneMonthAgo },
-//     });
-
-//     res.status(200).json({
-//       posts,
-//       totalPosts,
-//       lastMonthPosts,
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
